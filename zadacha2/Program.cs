d2e0aee313e4e6fbf0adf559c3569b6267b93687
Tuyana <tuyaochirova98@gmail.com>
2022-11-19 23:45:00 +0800

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Number1 = new int();
+            Console.Write("Введите певрое число: ");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            int Number2 = new int();
+            Console.Write("Введите второе число: ");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            int Number3 = new int();
+            Console.Write("Введите третье число: ");
+            Number3 = Convert.ToInt32(Console.ReadLine());
+            int max = Number1;
+            if (Number1 > max) max = Number1;
+            if (Number2 > max) max = Number2;
+            if (Number3 > max) max = Number3;
+            Console.WriteLine ($"Максимальное число: {max}");
+            
+        }
+    }
+
+}    
+
