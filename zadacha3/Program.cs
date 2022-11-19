584c0e87f777e60964a830c7462ae3159e3f131d
Tuyana <tuyaochirova98@gmail.com>
2022-11-20 00:03:17 +0800

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Number = new int();
+            Console.Write("Введите число: ");
+            Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0) 
+            {
+                Console.WriteLine("Число является чётным");
+            }
+            else
+            {
+                Console.WriteLine("Число не является чётным");
+            }
+        }
+    }
+
+}    
