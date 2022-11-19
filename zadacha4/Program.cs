fa415a6e8ed3586fcb1a0103e9eece02fa8ec804
Tuyana <tuyaochirova98@gmail.com>
2022-11-20 00:17:51 +0800

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zadacha4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = new int();
+            Console.Write("Введите число: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < N -1 )
+            {
+                i += 2;
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}    
