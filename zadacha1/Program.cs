c0d717a87c64bef3b0a9c51bb20e93655d101cfa
Tuyana <tuyaochirova98@gmail.com>
2022-11-19 23:24:12 +0800

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Number1 = new int();
+            Console.Write("Введите певрое число: ");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            int Number2 = new int();
+            Console.Write("Введите второе число: ");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            if(Number1 < Number2)
+            {
+                Console.WriteLine ($"Большее число: {Number2}");
+                Console.WriteLine ($"Меньшее число: {Number1}");
+            }
+            else
+            {
+                Console.WriteLine ($"Большее число: {Number1}");
+                Console.WriteLine ($"Меньшее число: {Number2}");
+            }    
+        }
+    }
+
+}    
+
